cc0dcd4ffd9974d37dc1ba9721d5a50d7c264df6
MG <mg84@bk.ru>
2020-03-01 17:48:40 +0300

    init app

---
+++ src/app/components/cesar/cesar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cesar',
+  templateUrl: './cesar.component.html',
+  styleUrls: ['./cesar.component.scss']
+})
+export class CesarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
