a29b13118b1ede4f9094a8b5a1ce3cdae849f783
MG <mg84@bk.ru>
2020-05-07 23:57:06 +0300

    Steganography in work

--- src/app/components/steganography/steganography.component.ts
+++ src/app/components/steganography/steganography.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core'
+import {SteganographyService} from '../../services/steganography.service'
 
 @Component({
   selector: 'app-steganography',
   templateUrl: './steganography.component.html',
   styleUrls: ['./steganography.component.scss']
 })
-export class SteganographyComponent implements OnInit {
+export class SteganographyComponent {
+  @ViewChild('canvas', {static: false}) canvas: ElementRef;
+  message: string = 'Hello!'
+  isCompleteEncoded: boolean = false
+  isCompleteDecoded: boolean = false
+  isUploadDisabled: boolean = false
+  image: string = ''
 
-  constructor() { }
+  constructor(private codeService: SteganographyService) {}
 
-  ngOnInit(): void {
+  messageInputHandler() {
+    this.isUploadDisabled = this.message.length <= 3
   }
 
+  async fileUploadHandler(e) {
+    const result = await this.codeService.encrypt(e.target.files[0], this.message)
+    this.image = 'data:image/png;base64,' + result
+    // console.log('data:image/png;base64,' + result)
+  }
 }
--- src/app/services/steganography.service.ts
+++ src/app/services/steganography.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SteganographyService {
 
-  constructor() { }
+  async encrypt(img: File, message: string) {
+
+    const messageView = this.getMessageView(message)
+    const imageView = await this.getImgView(img)
+    // console.log(imageView)
+
+
+    const decoder = new TextDecoder('utf8')
+    const a = decoder.decode(imageView)
+    console.log(a)
+    return btoa(unescape(encodeURIComponent(a)))
+  }
+
+  getMessageView(message: string): Uint8Array {
+    const encoder = new TextEncoder()
+    return encoder.encode(message)
+  }
+
+  getImgView(img: File): Promise<any> {
+    return new Promise<any>(done => {
+      const fr = new FileReader()
+      type EventReader = Event & {target: {result: ArrayBuffer}}
+      fr.addEventListener('load', (e: EventReader) => done(new Uint8ClampedArray(e.target.result)))
+      fr.readAsArrayBuffer(img)
+    })
+  }
+
+
 }
